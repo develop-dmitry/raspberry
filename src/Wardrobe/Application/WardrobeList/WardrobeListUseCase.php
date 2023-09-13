a2284388e536c216920c806c4a481a29c56c119b
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-09-13 21:52:16 +0300

    Refactoring wardrobe application

--- src/Wardrobe/Application/WardrobeList/WardrobeListUseCase.php
+++ src/Wardrobe/Application/WardrobeList/WardrobeListUseCase.php
@@ -8,10 +8,15 @@ use Raspberry\Wardrobe\Application\WardrobeList\DTO\ClothesData;
 use Raspberry\Wardrobe\Application\WardrobeList\DTO\WardrobeListRequest;
 use Raspberry\Wardrobe\Application\WardrobeList\DTO\WardrobeListResponse;
 use Raspberry\Wardrobe\Domain\Clothes\ClothesInterface;
+use Raspberry\Wardrobe\Domain\Wardrobe\Exceptions\UserDoesNotExistsException;
 use Raspberry\Wardrobe\Domain\Wardrobe\WardrobeRepositoryInterface;
 
 class WardrobeListUseCase implements WardrobeListInterface
 {
+
+    /**
+     * @param WardrobeRepositoryInterface $wardrobeRepository
+     */
     public function __construct(
         protected WardrobeRepositoryInterface $wardrobeRepository
     ) {
@@ -22,10 +27,20 @@ class WardrobeListUseCase implements WardrobeListInterface
      */
     public function execute(WardrobeListRequest $request): WardrobeListResponse
     {
-        $wardrobe = $this->wardrobeRepository->getWardrobe($request->userId);
-        $items = $wardrobe->getClothes();
+        $items = $this->getWardrobeClothes($request->userId);
         $items = array_map(static fn (ClothesInterface $clothes) => ClothesData::fromDomain($clothes), $items);
 
         return new WardrobeListResponse(items: $items);
+    }
+
+    /**
+     * @return ClothesInterface[]
+     * @throws UserDoesNotExistsException
+     */
+    protected function getWardrobeClothes(int $userId): array
+    {
+        $wardrobe = $this->wardrobeRepository->getWardrobe($userId);
+
+        return $wardrobe->getClothes();
     }
 }
--- src/Wardrobe/Application/WardrobeOffers/WardrobeOffersUseCase.php
+++ src/Wardrobe/Application/WardrobeOffers/WardrobeOffersUseCase.php
@@ -4,18 +4,25 @@ declare(strict_types=1);
 
 namespace Raspberry\Wardrobe\Application\WardrobeOffers;
 
+use Raspberry\Core\Pagination\PaginationInterface;
 use Raspberry\Wardrobe\Application\WardrobeOffers\DTO\ClothesData;
 use Raspberry\Wardrobe\Application\WardrobeOffers\DTO\WardrobeOffersRequest;
 use Raspberry\Wardrobe\Application\WardrobeOffers\DTO\WardrobeOffersResponse;
 use Raspberry\Wardrobe\Domain\Clothes\ClothesInterface;
+use Raspberry\Wardrobe\Domain\Wardrobe\Exceptions\UserDoesNotExistsException;
 use Raspberry\Wardrobe\Domain\Wardrobe\Services\Offers\OffersServiceInterface;
 use Raspberry\Wardrobe\Domain\Wardrobe\WardrobeRepositoryInterface;
 
 class WardrobeOffersUseCase implements WardrobeOffersInterface
 {
+
+    /**
+     * @param WardrobeRepositoryInterface $wardrobeRepository
+     * @param OffersServiceInterface $offersService
+     */
     public function __construct(
         protected WardrobeRepositoryInterface $wardrobeRepository,
-        protected OffersServiceInterface $wardrobeOffersService
+        protected OffersServiceInterface $offersService
     ) {
     }
 
@@ -24,17 +31,30 @@ class WardrobeOffersUseCase implements WardrobeOffersInterface
      */
     public function execute(WardrobeOffersRequest $request): WardrobeOffersResponse
     {
-        $wardrobe = $this->wardrobeRepository->getWardrobe($request->userId);
-        $offers = $this->wardrobeOffersService->getOffers($wardrobe, $request->page, $request->count);
+        $offersPagination = $this->getWardrobeOffers($request->userId, $request->page, $request->count);
 
-        $items = $offers->getItems();
+        $items = $offersPagination->getItems();
         $items = array_map(static fn (ClothesInterface $clothes) => ClothesData::fromDomain($clothes), $items);
 
         return new WardrobeOffersResponse(
             items: $items,
-            page: $offers->getPage(),
-            total: $offers->getTotal(),
-            count: $offers->getPerPage()
+            page: $offersPagination->getPage(),
+            total: $offersPagination->getTotal(),
+            count: $offersPagination->getPerPage()
         );
+    }
+
+    /**
+     * @param int $userId
+     * @param int $page
+     * @param int $count
+     * @return PaginationInterface
+     * @throws UserDoesNotExistsException
+     */
+    public function getWardrobeOffers(int $userId, int $page, int $count): PaginationInterface
+    {
+        $wardrobe = $this->wardrobeRepository->getWardrobe($userId);
+
+        return $this->offersService->getOffers($wardrobe, $page, $count);
     }
 }
